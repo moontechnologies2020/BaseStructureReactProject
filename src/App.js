87818bf317c98b1e5f6dbe9b4cce40fd54c968b7
Moon Technologies <moon.technologies2020@gmail.com>
2019-10-26 00:21:30 -0500

    Structure Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <>
+      <h1>Tu código aquí</h1>
+      <p>En este archivo escribes tu código inicial en React.</p>
+    </>
+  )
+}
